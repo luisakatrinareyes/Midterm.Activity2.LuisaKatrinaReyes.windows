fb9c6a6cb395cc8a4272d5c66ee2e93d21a5fd9e
Luisa Katrina Reyes <luisakatrinareyes@gmail.com>
2019-09-18 17:28:50 +0800

    Creating the Design  of Loops

---
+++ Midterm.Activity2.LuisaKatrinaReyes/Midterm.Activity2.LuisaKatrinaReyes.windows/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Midterm.Activity2.LuisaKatrinaReyes.windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
